7696cacb975874c35ab9f4f52e1eb5dc4e3dfdd0
xgan68 <ganx-68@live.cn>
2020-02-04 15:23:04 +0800

    清理代码

--- Assets/Source/Definition/Const.Notification.cs
+++ Assets/Source/Definition/Const.Notification.cs
@@ -6,35 +6,30 @@ public static partial class Const
 {
     public static class Notification
     {
+        #region Http request
         public const string QR_SCAN_LOGIN = "QrScanLogin";
         public const string LOGIN_SUCCESS = "LoginSuccess";
         public const string LOGIN_FAIL = "LoginFail";
         public const string LOGOUT = "Logout";
         public const string LOGOUT_SUCCESS = "LogoutSuccess";
         public const string CHECK_LOGIN_STATUS = "CheckLoginStatus";
+        public const string CHECK_USER_INFO_EXIST = "CheckUserInfo";
+        public const string SUBMIT_USER_INFO = "SubmitUserInfo";
+        #endregion
+
+        #region Local system
         public const string LOAD_UI_FORM = "LoadUIForm";
         public const string LOAD_UI_ROOT_FORM = "LoadUIRootForm";
         public const string BACK_TO_LAST_FORM = "BackToLastForm";
         public const string GO_TO_HOME_FORM = "GoToHomeForm";
-
-        public const string WS_SEND = "WsSend";
-
-        public const string LOAD_SCENE = "LoadScene";
-
-        public const string CHECK_USER_INFO_EXIST = "CheckUserInfo";
-        public const string SUBMIT_USER_INFO = "SubmitUserInfo";
-        public const string SETUP_CONNECTION_WITH_SERVER = "SetupConnectionWithServer";
-
-        //old, cc code ood 
-        public const string SEND_LOGIN = "SendLogin";
-        public const string CONNECT_TO_WS_SERVER = "ConnectToWsServer";
-        public const string CHANGE_GAME_STATUS = "ChangeGameStatus";
-        public const string GAME_STATUS_CHANGED = "GameStatusChanged";
-        public const string GAME_STATUS_CHANGE_ERROR = "GameStatusChangeError";
-        public const string UPDATE_DEVICE_ID_TO_USER_INFO = "UpdateDeviceIdToUserInfo";
-        public const string REQUEST_FOR_GAME_STATUS = "RequestForGameSessions";
-        public const string RECEIVED_GAME_STATUS = "ReceivedGameStatus";
         public const string GAME_STARTED = "GameStarted";
-        public const string GAME_CLOSED = "GameClosed";
+        public const string LOAD_SCENE = "LoadScene";
+        #endregion
+
+        #region Communication
+        public const string WS_SEND = "WsSend";
+        public const string CONNECT_TO_WS_SERVER = "ConnectToWsServer";
+        public const string SETUP_CONNECTION_WITH_SERVER = "SetupConnectionWithServer";
+        #endregion
     }
 }
