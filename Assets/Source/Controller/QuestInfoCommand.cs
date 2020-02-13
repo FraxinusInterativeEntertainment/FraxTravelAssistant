d371b4311dd58ce15e6a3e1083e619e72c70d703
LiuqiInHome <liuqi_fraxinus@163.com>
2020-02-13 16:40:17 +0800

    旅行辅助系统的任务提示

---
+++ Assets/Source/Controller/QuestInfoCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PureMVC.Patterns;
+using PureMVC.Interfaces;
+
+public class QuestInfoCommand : SimpleCommand
+{
+
+    public override void Execute(INotification _notification)
+    {
+     
+    }
+}
